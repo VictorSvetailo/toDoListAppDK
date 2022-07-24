a5c9782930c8c0589da1536e93237b73b93f225a
VictorSvetailo <victorioeve@gmail.com>
2022-07-24 12:17:37 +0300

    Commit function 'map' #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './App.css';
-import TodoList, {TaskType} from "./TodoList";
+import {TodoList} from "./TodoList";
+
+
+const tasks1 = [
+    {id: 1, title: 'HTML&CSS', isDone: true},
+    {id: 2, title: 'JS', isDone: true},
+    {id: 3, title: 'ReactJS', isDone: false},
+    {id: 4, title: 'Redux', isDone: false},
+]
+const tasks2 = [
+    {id: 1, title: 'Hello world', isDone: true},
+    {id: 2, title: 'I am Happy', isDone: false},
+    {id: 3, title: 'Yo', isDone: false},
+]
 
 function App() {
-    const tasks1: Array<TaskType> = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false},
-        {id: 4, title: 'Redux', isDone: false},
-    ]
-    const tasks2: Array<TaskType> = [
-        {id: 1, title: 'Hello world', isDone: true},
-        {id: 2, title: 'I am Happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
     return (
         <div className="App">
             <TodoList title={'What to learn'} tasks={tasks1}/>
