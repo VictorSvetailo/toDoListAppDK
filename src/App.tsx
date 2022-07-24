af1693c51b9754c564998aa3a093c363098e2d6e
VictorSvetailo <victorioeve@gmail.com>
2022-07-24 12:14:11 +0300

    Commit function 'map'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const tasks1: Array<TaskType> = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+    ]
+    const tasks2: Array<TaskType> = [
+        {id: 1, title: 'Hello world', isDone: true},
+        {id: 2, title: 'I am Happy', isDone: false},
+        {id: 3, title: 'Yo', isDone: false},
+    ]
+    return (
+        <div className="App">
+            <TodoList title={'What to learn'} tasks={tasks1}/>
+            <TodoList title={'Songs'} tasks={tasks2}/>
+            {/*<TodoList title={'What to read'} tasks={tasks}/>*/}
+        </div>
+    );
 }
 
 export default App;
